8da85bdf3906f530f1b1bec4b3250e1a6b1e5bbf
wsanchezdupont1 <wsanchezdupont@g.hmc.edu>
2019-12-29 23:46:01 +0100

    backward hooks now able to edit grad

--- hooktest.py
+++ hooktest.py
@@ -3,18 +3,26 @@ import torch
 def hookfoo(mod,gradin,gradout):
     print('module =',mod)
     for i in range(len(gradin)):
+        print('len(gradin) =',len(gradin))
         print('\ngradin[{}].shape ='.format(i),gradin[i].shape)
         print('gradin[{}] ='.format(i),gradin[i])
+
+        # print('\n\n\n\n')
+        # print('gradin =',gradin)
+        # print('\n\n\n\n')
 
     # for i in range(len(gradout)):
     #     print('gradout[{}].shape ='.format(i),gradout[i].shape)
     #     print('gradout[{}] ='.format(i),gradout[i])
 
-x = torch.ones(8,4,requires_grad=True)*2
+    # return (gradin[0], torch.nn.ReLU()(gradin[1]), gradin[2])
+
+x = 2*torch.ones(8,4)
+x.requires_grad = True
 
 n = torch.nn.Linear(4,5)
 for p in n.parameters():
-    p.data = torch.ones_like(p.data)
+    p.data = -1*torch.ones_like(p.data)
 
 h = n.register_backward_hook(hookfoo)
 
@@ -24,6 +32,7 @@ print('\ny =',y)
 print('x.grad =',x.grad)
 print('\n\n\n\n')
 
+h.remove()
 
 # a = torch.ones(8,4,requires_grad=True)
 # n(a).sum().backward()
@@ -43,10 +52,14 @@ class Foo(torch.nn.Module):
         return self.fc1(x1),self.fc2(x2)
 
 f = Foo()
-x1 = torch.ones(1,4,requires_grad=True)*3
-x2 = torch.ones(1,5,requires_grad=True)*4
+x1 = torch.ones(1,4)*3
+x2 = torch.ones(1,5)*4
+x1.requires_grad = True
+x2.requires_grad = True
 
 h = f.register_backward_hook(hookfoo)
 
 y,z = f(x1,x2)
 (y.sum() + z.sum()).backward()
+print('x1.grad =',x1.grad)
+print('x2.grad =',x2.grad)
